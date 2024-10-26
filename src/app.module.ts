bd9543e10f672b319b14d170897863a3a8d838e5
murphyyue <murphyyue@icloud.com>
2024-10-26 18:37:21 +0800

    change db form pg to mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,28 +5,15 @@ import { AuthModule } from './auth/auth.module';
 import { AuthService } from './auth/auth.service';
 
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        // TODO: set to false in production
-        synchronize: true,
-      }),
-    }),
     EmotionRecordModule,
+    DatabaseModule,
     // AuthModule,
   ],
   // providers: [AuthService],
