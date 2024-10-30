8232af847fba3b87ff6c016b40890d28b02e2b81
murphyyue <murphyyue@icloud.com>
2024-10-30 23:20:38 +0800

    update main.ts to enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001', // Frontend origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
+    credentials: true, // If your frontend needs to send cookies
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
